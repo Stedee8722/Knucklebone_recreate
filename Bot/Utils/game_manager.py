01a19a4589175b3efe98b02be05a07b4050d4cab
Stedee <doanthanhbnh@gmail.com>
2025-10-10 08:57:16 +0700

    logging info for game manager

--- Bot/Utils/game_manager.py
+++ Bot/Utils/game_manager.py
@@ -1,3 +1,5 @@
+import main, inspect
+
 active_games = []
 
 def is_active(identifier: str) -> bool:
@@ -6,8 +8,29 @@ def is_active(identifier: str) -> bool:
 
 def add_game(identifier: str):
     """Register a new active game."""
+    # Get the current frame
+    current_frame = inspect.currentframe()
+    # Get the outer frame (the caller's frame)
+    caller_frame = current_frame.f_back
+    # Get the code object of the caller's frame
+    caller_code = caller_frame.f_code
+    # Get the name of the caller's method
+    caller_name = caller_code.co_name
+    main.logger.info(f"Adding game {identifier} to active games list.\n Active games: {active_games}\n From: {caller_name}")
     active_games.append(identifier)
 
 def remove_game(identifier: str):
     """Remove a finished game."""
-    active_games.remove(identifier)
+    main.logger.info(f"Removing game {identifier} to active games list.\n Active games: {active_games}")
+    try: active_games.remove(identifier)
+    except Exception as exception:
+        # Get the current frame
+        current_frame = inspect.currentframe()
+        # Get the outer frame (the caller's frame)
+        caller_frame = current_frame.f_back
+        # Get the code object of the caller's frame
+        caller_code = caller_frame.f_code
+        # Get the name of the caller's method
+        caller_name = caller_code.co_name
+        main.logger.error(f"Failed to remove game {identifier} from active games list. \nError: {exception}\n Active games: {active_games}\n From: {caller_name}")
+        pass
